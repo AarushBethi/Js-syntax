b4fc6cf5a81577b4b72d0c2ae698908cda4802a8
AarushBethi <128331842+AarushBethi@users.noreply.github.com>
2023-08-27 19:54:53 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var first_name = 'Aarush'
+var second_name = 'Bethi'
+console.log(first_name)
+console.log(second_name)
+var full_name = first_name.concat(second_name)
+console.log(full_name)
+var word = "I am Aarush Bethi"
+var rep = word.replace("Aarush","Saanvi")
+console.log(rep)
+var len= full_name.length
+console.log(len)
+var caps = full_name.toUpperCase()
+console.log(caps)
+var lower = full_name.toLowerCase()
+console.log(lower)
+var number = Math.sqrt(1450)
+console.log(number)
+var power = Math.pow(8,5)
+console.log(power)
+var num1 = 10
+var num2 = 15
+var add = num1 + num2
+console.log(add)
+var sub = num1 - num2
+console.log(sub)
+var div = num1/num2
+console.log(div)
+var mul = num1 * num2
+console.log(mul)
